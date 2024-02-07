3a06203b0fd70140399e3a8cb04480e4d7f39d10
yuansl <yuanshenglong@126.com>
2024-02-07 08:59:42 +0800

    c: remove some.c && add some snippets

--- c/main.c
+++ c/main.c
@@ -1,7 +1,18 @@
 #define _GNU_SOURCE
-#include <linux/limits.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h> /* for getaddrinfo */
+#include <complex.h>
+#include <time.h>
+#include <limits.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdatomic.h>
+#include <stdint.h>
+#include <unistd.h> /* for close */
+#include <stdint.h>
+#include <inttypes.h>
+#include <pthread.h>
 
 #include <jansson.h>
 #include "util.h"
@@ -10,6 +21,389 @@
 #include "any.h"
 
 #define STRING_BUFSIZE 10
+
+#include "option.h"
+
+#define IPV4_DOT_FORM_MAX_LEN 16
+
+#define __noreturn	      __attribute__((noreturn))
+#define __unused	      __attribute__((unused))
+
+static inline void __nonnull((1)) inspect_sockaddr(struct sockaddr_in *inaddr)
+{
+	char ipbuf[IPV4_DOT_FORM_MAX_LEN];
+
+	inet_ntop(inaddr->sin_family, &inaddr->sin_addr, ipbuf, sizeof(ipbuf));
+
+	printf("connect to %s:%d\n", ipbuf, ntohs(inaddr->sin_port));
+}
+
+static int __nonnull((2))
+	do_connect(int sockfd, const char *domain, const char *service)
+{
+	struct addrinfo *addrs;
+	bool found;
+	int err;
+
+	if ((err = getaddrinfo(domain, service, NULL, &addrs)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
+		return -1;
+	}
+
+	found = false;
+	for (struct addrinfo *ai = addrs; ai != NULL; ai = ai->ai_next) {
+		inspect_sockaddr((struct sockaddr_in *)ai->ai_addr);
+
+		if (connect(sockfd, ai->ai_addr, ai->ai_addrlen) == 0) {
+			found = true;
+			break;
+		}
+	}
+	freeaddrinfo(addrs);
+
+	return found ? 0 : -1;
+}
+
+static int initialize_connection(const char *domain, const char *service)
+{
+	int sockfd;
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		return -1;
+	}
+	if (do_connect(sockfd, domain, service) < 0) {
+		close(sockfd);
+		return -1;
+	}
+	return sockfd;
+}
+
+#define NR_FLOATS 2
+
+static void send_http_request(int connfd, const char host[static 1])
+{
+	char buf[BUFSIZ];
+	ssize_t nbytes;
+
+	sprintf(buf,
+		"GET /index.html HTTP/1.1\r\nHost: %s\r\nUser-Agent: Simple-http/1.0\r\n\r\n",
+		host);
+	nbytes = strlen(buf);
+
+	printf("request(raw): '%s'\n", buf);
+
+	if (write(connfd, buf, nbytes) != nbytes)
+		fatal("write: %m\n");
+
+	if ((nbytes = read(connfd, buf, sizeof(buf))) < 0)
+		fatal("read: %m\n");
+	buf[nbytes] = '\0';
+
+	printf("response(raw)='%s'\n", buf);
+}
+
+static void __unused test_http_request(void)
+{
+	const char *host = "www.qiniu.com";
+	int connfd;
+
+	connfd = initialize_connection(host, "http");
+	if (connfd < 0)
+		fatal("initialize_socket: %m\n");
+
+	send_http_request(connfd, host);
+
+	close(connfd);
+}
+
+typedef unsigned char byte;
+
+struct sdshdr8 {
+	uint8_t len;   /* used */
+	uint8_t alloc; /* excluding the header and null terminator */
+	byte flags;    /* 3 lsb of type, 5 unused bits */
+	char buf[];
+} __attribute__((__packed__));
+
+typedef struct sdshdr8 sdshdr_t;
+
+void test_sdshdr(void)
+{
+	sdshdr_t *shdr;
+	byte *s;
+
+	shdr = malloc(sizeof(sdshdr_t) + 1);
+	s    = (byte *)shdr + sizeof(sdshdr_t);
+	s[0] = '\0';
+	free(shdr);
+}
+
+#define bitsizeof(x) (CHAR_BIT * sizeof(x))
+
+#define maximum_unsigned_value_of_type(a) \
+	(UINTMAX_MAX >> (bitsizeof(uintmax_t) - bitsizeof(a)))
+
+#define unsigned_add_overflows(a, b) \
+	((b) > maximum_unsigned_value_of_type(a) - (a))
+
+#define die(...) fatal(__VA_ARGS__)
+
+#define xcalloc	 calloc
+
+static inline size_t st_add(size_t a, size_t b)
+{
+	if (unsigned_add_overflows(a, b))
+		die("size_t overflow: %" PRIuMAX " + %" PRIuMAX, (uintmax_t)a,
+		    (uintmax_t)b);
+	return a + b;
+}
+#define st_add3(a, b, c) st_add(st_add((a), (b)), (c))
+
+#define FLEX_ALLOC_MEM(x, flexname, buf, len)                                \
+	do {                                                                 \
+		size_t flex_array_len_ = (len);                              \
+		(x) = xcalloc(1, st_add3(sizeof(*(x)), flex_array_len_, 1)); \
+		memcpy((void *)(x)->flexname, (buf), flex_array_len_);       \
+	} while (0)
+#define FLEXPTR_ALLOC_MEM(x, ptrname, buf, len)                              \
+	do {                                                                 \
+		size_t flex_array_len_ = (len);                              \
+		(x) = xcalloc(1, st_add3(sizeof(*(x)), flex_array_len_, 1)); \
+		memcpy((x) + 1, (buf), flex_array_len_);                     \
+		(x)->ptrname = (void *)((x) + 1);                            \
+	} while (0)
+#define FLEX_ALLOC_STR(x, flexname, str) \
+	FLEX_ALLOC_MEM((x), flexname, (str), strlen(str))
+#define FLEXPTR_ALLOC_STR(x, ptrname, str) \
+	FLEXPTR_ALLOC_MEM((x), ptrname, (str), strlen(str))
+
+struct array {
+	size_t alloc;
+	size_t nr;
+	char name[];
+};
+
+void test_array(void)
+{
+	struct array *f;
+
+	FLEX_ALLOC_STR(f, name, "something");
+
+	free(f);
+}
+
+struct options {
+	int age;
+	const char *name;
+};
+
+static void options_set_age(option_t opt, void *options)
+{
+	((struct options *)options)->age = *(int *)opt->data;
+}
+
+static void options_set_name(option_t opt, void *options)
+{
+	((struct options *)options)->name = opt->data;
+
+	/* Ok, we reuse opt->data here, avoid allocating memory again */
+	opt->data = NULL;
+}
+
+option_t with_age(int age)
+{
+	option_t opt = malloc(sizeof(struct option));
+
+	*opt = (struct option){
+		.apply = options_set_age,
+		.data  = malloc(sizeof(age)),
+	};
+
+	*(int *)opt->data = age;
+
+	return opt;
+}
+
+option_t with_name(const char *name)
+{
+	option_t opt = malloc(sizeof(struct option));
+
+	opt->apply = options_set_name;
+	opt->data  = malloc(strlen(name) + 1);
+	strcpy((char *)opt->data, name);
+
+	return opt;
+}
+
+void init_options_internal(struct options *options, /* option_t opts */...)
+{
+	va_list ap;
+
+	va_start(ap, options);
+	while (true) {
+		option_t opt = va_arg(ap, option_t);
+		if (!opt)
+			break;
+		opt->apply(opt, options);
+
+		option_destroy(opt);
+	}
+	va_end(ap);
+}
+
+#define init_options(opts, ...) init_options_internal(opts, __VA_ARGS__)
+
+void with_retry(int retry_max, int (*f)(void))
+{
+	int attempts = 0;
+
+	do {
+		if (f() == 0)
+			break;
+		attempts++;
+	} while (attempts <= retry_max);
+}
+
+static void __unused test_options(void)
+{
+	struct options options = {};
+
+	init_options(&options, with_age(38), with_name("liming"), NULL);
+
+	printf("age=%d, name='%s'\n", options.age, options.name);
+}
+
+thread_local int some_key;
+
+void *print_some(void *)
+{
+	for (int i = 0; i < 10; i++) {
+		printf("print thread %ld: thread_local key=%d\n",
+		       pthread_self() % 1000'000, some_key);
+		nanosleep(&(static struct timespec){ .tv_sec = 1 }, NULL);
+	}
+	pthread_exit(NULL);
+}
+
+void *inc_some(void *)
+{
+	for (int i = 0; i < 10; i++) {
+		printf("inc thread %ld: thread_local key=%d\n",
+		       pthread_self() % 1000'000, some_key);
+		some_key++;
+		nanosleep(&(static struct timespec){ .tv_nsec = 1000000 },
+			  NULL);
+	}
+	pthread_exit(NULL);
+}
+
+#define NR_THREADS 2
+
+void test_pthreads(void)
+{
+	pthread_t threads[NR_THREADS];
+
+	pthread_create(&threads[0], NULL, print_some, NULL);
+	pthread_create(&threads[1], NULL, inc_some, NULL);
+
+	for (int i = 0; i < NR_THREADS; i++) {
+		pthread_join(threads[i], NULL);
+	}
+}
+
+struct point {
+	double x, y;
+};
+
+union image {
+	double pixel;
+};
+
+struct rectangle {
+	struct point center;
+	double width, height;
+};
+
+struct circle {
+	struct point center;
+	double r;
+};
+
+__nonnull((1)) static void scale_rectange_1p(struct rectangle *rec,
+					     double scale)
+{
+	rec->width *= scale;
+	rec->height *= scale;
+}
+
+static void __nonnull((1))
+	scale_rectange_2p(struct rectangle *rec, double h_scale, double w_scale)
+{
+	rec->width *= w_scale;
+	rec->height *= h_scale;
+}
+
+static void __nonnull((1)) scale_circle(struct circle *c, double scale)
+{
+	c->r *= scale;
+}
+
+/* clang-format off */
+
+#define __scale2p(obj, ...)				\
+	_Generic(obj,					\
+		 struct rectangle*:scale_rectange_2p	\
+                )(obj, __VA_ARGS__)
+
+#define __scale1p(obj, ...)				\
+	_Generic(obj,					\
+	         struct circle*:scale_circle,		\
+		 struct rectangle*:scale_rectange_1p	\
+                )(obj, __VA_ARGS__)
+
+#define __INVOKE_SCALE(_1,_2,_3,NAME,...) NAME 
+	
+#define scale(...) __INVOKE_SCALE(__VA_ARGS__, __scale2p, __scale1p)(__VA_ARGS__)
+
+/* clang-format on */
+
+struct array2 {
+	int size;
+	int cap;
+	char buf[];
+};
+
+#define IMAGE(NAME, ...)    \
+	(union image)       \
+	{                   \
+		__VA_ARGS__ \
+	}
+
+constexpr int NR_NUMBERS = 10;
+
+void test_scale(void)
+{
+	union image img = IMAGE(.pixel = 3);
+	(void)img;
+	long double *numbers = calloc(NR_NUMBERS, sizeof(*numbers));
+	struct array2 x	     = (static struct array2){ .buf = "hello, world" };
+
+	printf("sizeof(double[12]) = %zd\n", sizeof(double[12]));
+	(void)x;
+
+	for (int i = 0; i < NR_NUMBERS; i++)
+		numbers[i] = rand() % 100;
+
+	struct rectangle r = { .height = 1, .width = 2 };
+	struct circle c	   = { .r = 5 };
+
+	scale(&r, 3.8, 3.5);
+	scale(&c, 4.2);
+
+	printf("rectange.height=%.2f,wigth=%.2f, circle.r=%.2f\n", r.height,
+	       r.width, c.r);
+}
 
 /* maybe unused */
 #define __unused __attribute__((unused))
@@ -117,15 +511,21 @@ void test_stringbuffer(void)
 	buffer_destroy(array);
 }
 
+#ifndef NAME_MAX
+#define NAME_MAX 256
+#endif
+
+enum gender : uint8_t {
+	MALE,
+	FEMALE
+};
+
 struct person {
 	char name[NAME_MAX];
 	int age;
 	char blog[NAME_MAX];
 	char addr[NAME_MAX];
-	enum {
-		MALE,
-		FEMALE
-	} gender;
+	enum gender gender;
 };
 
 static void person_pretty_print(struct person *p)
@@ -207,12 +607,216 @@ void test_any_struct(void)
 	printf("sizeof(p)=%zd,alignof=%zd\n", sizeof(liulei), alignof(liulei));
 }
 
+struct shared {
+	_Atomic int count;
+	_Atomic bool done;
+};
+
+struct thread {
+	struct shared *shared;
+	pthread_t id;
+};
+
+void *count_self(void *arg)
+{
+	struct thread *thread = arg;
+
+	nanosleep(&(struct timespec){ .tv_nsec = rand() % 1000 }, NULL);
+
+	thread->shared->count++;
+
+	if (pthread_self() % 2 == 0) {
+		printf("in thread %ld, count now = %d\n",
+		       pthread_self() % NR_THREADS,
+		       atomic_load(&thread->shared->count));
+
+		thread->shared->done = true;
+	}
+
+	pthread_exit(NULL);
+}
+
+void test_atomic_variable(void)
+{
+	struct shared shared		  = {};
+	struct thread threads[NR_THREADS] = {
+		[0 ... NR_THREADS - 1].shared = &shared,
+	};
+
+	for (int i = 0; i < NR_THREADS; i++) {
+		int e;
+		threads[i].shared = &shared;
+		e = pthread_create(&threads[i].id, NULL, count_self,
+				   &threads[i]);
+		if (e)
+			fatal("pthread_create: %s\n", strerror(e));
+	}
+
+	for (int i = 0; i < NR_THREADS; i++) {
+		pthread_join(threads[i].id, NULL);
+	}
+
+	if (shared.count != NR_THREADS)
+		printf("Oops: shared.count=%d, expected = %d\n", shared.count,
+		       NR_THREADS);
+}
+
+struct refcount {
+	_Atomic int count;
+};
+
+#define REFCOUNT_DEC(ref)  --(ref).count
+#define REFCOUNT_INCR(ref) (ref).count++
+
+struct some {
+	char name[256];
+	int age;
+	enum gender gender;
+	struct refcount refcount;
+};
+
+struct some *clone_some(struct some *some)
+{
+	REFCOUNT_INCR(some->refcount);
+	return some;
+}
+
+static struct some *some_init(struct some *some)
+{
+	*some = (struct some){
+		.age		= 26,
+		.name		= "hello",
+		.gender		= MALE,
+		.refcount.count = 1,
+	};
+	return some;
+}
+
+void some_destory(void *some)
+{
+	struct some *_some = *(struct some **)some;
+
+	fprintf(stderr, "hello, some\n");
+
+	if (REFCOUNT_DEC(_some->refcount) <= 0) {
+		printf("OK, some will be freed\n");
+		free(_some);
+	}
+}
+
+void cleanup_x(void *x)
+{
+	struct some *some = x;
+
+	if (--some->refcount.count <= 0) {
+		printf("now , variable x(struct some*) will be freed");
+		free(some);
+	}
+
+	printf("hello, this is a cleanup function %d\n", *(int *)x);
+}
+
+#define lock(x) atomic_compare_exchange_strong(&x, &(int){ 0 }, 1)
+
+void unlock(void *x)
+{
+	printf("unlock (x)...\n");
+	atomic_compare_exchange_strong((_Atomic int *)x, &(int){ 1 }, 0);
+}
+
+double chainable_division(double x, double y)
+{
+	return x / y;
+}
+
+#define __native_word(t)                                            \
+	(sizeof(t) == sizeof(char) || sizeof(t) == sizeof(short) || \
+	 sizeof(t) == sizeof(int) || sizeof(t) == sizeof(long))
+
+enum kind {
+	KIND_ENUM	      = 1,
+	KIND_POINTER_OR_ARRAY = 5,
+	KIND_DECIMAL	      = 8,
+	KIND_COMPLEX	      = 9,
+	KIND_STRUCT	      = 12,
+	KIND_UNION	      = 13
+};
+
 int main(void)
 {
-	char a[0];
+	struct some *b;
+	{
+		struct some *some __attribute__((cleanup(some_destory)));
+		some = malloc(sizeof(*some));
 
-	printf("sizeof(a)=%zd, &a[0]=%p, alignof(a)=%zd\n", sizeof a, a,
-	       alignof(a));
+		some_init(some);
+
+		b = clone_some(some);
+
+		printf("some={.age=%d,.name='%s'.refcount=%d}\n", some->age,
+		       some->name, some->refcount.count);
+	}
+	size_t b_size = __builtin_object_size(b, 0);
+	int type      = __builtin_classify_type(b);
+	printf("b_size=%zd, sizeof *b=%zd, b={.age=%d,.name='%s',refcount=%d},native_type(*b)=%s, alignof(b)=%zd, type(b)=%d\n",
+	       b_size, sizeof(*b), b->age, b->name, b->refcount.count,
+	       __native_word(*b) ? "true" : "false", alignof(struct some *),
+	       type);
+
+	{
+		_Atomic int x __attribute__((cleanup(unlock))) = 0;
+		if (lock(x)) {
+			printf("lock acuired\n");
+		}
+	}
+	if (isinf(4 / 0.0))
+		printf("4/0 is inf\n");
+
+	double x = chainable_division(2.0, chainable_division(4, 0.0));
+	char *px = (char *)&x;
+	printf("x = %.2f\n", x);
+
+	for (int i = 0; i < (int)sizeof(x); i++) {
+		px[i] = 0xff;
+	}
+
+	if (isnan(x))
+		printf("now x is Nan\n");
+
+	{
+		struct V {
+			char buf1[10];
+			int b;
+			char buf2[10];
+		} var;
+		void *p = &var.buf1[1], *q = &var.b;
+
+		(void)p;
+
+		bool k	       = true;
+		enum kind k2   = KIND_ENUM;
+		int k3	       = 0;
+		long k4	       = 0L;
+		char k5	       = 'a';
+		complex k6     = 0;
+		_Decimal32 k7  = 0;
+		_Decimal64 k8  = 0;
+		_Decimal128 k9 = 0;
+		typedef void(func)(void);
+		func *k11;
+		union {
+			int a;
+			double b;
+			long c;
+			char d[256];
+		} k12	    = {};
+		int k13[12] = { [0 ... 9] = 1 };
+		char *k14   = "hello";
+
+		printf("address q=%p,&var=%p, %zd, typeof(struct V)=%d\n", q,
+		       &var, (char *)(&var + 1) - (char *)q,
+		       __builtin_classify_type(k14));
+	}
 
 	return 0;
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/yuansl/playground
 
-go 1.21
+go 1.22
 
 require (
 	github.com/Comcast/go-leaderelection v0.0.0-20211210163058-1d7a4eade3f5
@@ -8,7 +8,7 @@ require (
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/go-zookeeper/zk v1.0.2
 	github.com/godaner/awdb-golang v0.0.0-20230327050545-2904112c8a45
-	github.com/golang/mock v1.6.0
+	github.com/google/uuid v1.3.1
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.0
 	github.com/hazelcast/hazelcast-go-client v1.4.0
 	github.com/huaweicloud/huaweicloud-sdk-go-obs v3.23.9+incompatible
@@ -16,8 +16,8 @@ require (
 	github.com/jlaffaye/ftp v0.2.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/json-iterator/go v1.1.12
+	github.com/marcboeker/go-duckdb v1.5.6
 	github.com/parquet-go/parquet-go v0.0.0-20230713212203-3d3ee2af9ab7
-	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.17.0
 	github.com/qbox/net-deftones v1.2.0
 	github.com/qiniu/go-sdk/v7 v7.17.0
@@ -35,6 +35,7 @@ require (
 	go.uber.org/ratelimit v0.3.0
 	golang.org/x/net v0.18.0
 	golang.org/x/sync v0.3.0
+	golang.org/x/sys v0.14.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20231016165738-49dd2c1f3d0b
 	google.golang.org/grpc v1.59.0
 	google.golang.org/protobuf v1.31.0
@@ -61,13 +62,11 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/s2a-go v0.1.4 // indirect
-	github.com/google/uuid v1.3.1 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
-	github.com/marcboeker/go-duckdb v1.5.6 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
@@ -75,6 +74,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pierrec/lz4/v4 v4.1.18 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
 	github.com/prometheus/common v0.44.0 // indirect
@@ -94,7 +94,6 @@ require (
 	go.opentelemetry.io/otel/metric v1.19.0 // indirect
 	golang.org/x/crypto v0.15.0 // indirect
 	golang.org/x/oauth2 v0.12.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/api v0.128.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
