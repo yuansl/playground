5fb3d8428b31dcbb9d77ac1034120efe89e0f1fc
yuansl <yuanshenglong@126.com>
2024-03-20 09:06:19 +0800

    cmd/myraft: tiny refactor

--- cmd/myraft/main.go
+++ cmd/myraft/main.go
@@ -31,6 +31,30 @@ const (
 	LEADER_STATE
 )
 
+type Node struct {
+	Addr string
+	Name string
+}
+
+type VoteStatus int
+
+const (
+	VoteProgressing VoteStatus = iota
+	VoteWin
+)
+
+func (node *Node) SendVoteRequest(addr string, req *VoteRequest) (*VoteResponse, error) {
+	return &VoteResponse{Peer: &Node{Name: "B"}, Voted: true}, nil
+}
+
+func (node *Node) SendHealthcheckRequest(term int) (*AppendLogResponse, error) {
+	return node.SendAppendLogRequest(&AppendLogRequest{Term: term, Commited: true})
+}
+
+func (node *Node) SendAppendLogRequest(req *AppendLogRequest) (*AppendLogResponse, error) {
+	return &AppendLogResponse{Accepted: true}, nil
+}
+
 type VoteRequest struct {
 	Term int
 	From *Node
@@ -69,6 +93,8 @@ type Server struct {
 	state         State
 	Term          int
 	Others        []*Node
+	election      *time.Timer
+	healthcheck   *time.Ticker
 	appendlogChan chan *AppendLogRequest
 	voteFor       string
 	commited      []LogEntry
@@ -77,61 +103,48 @@ type Server struct {
 
 // HandleVoteRequest implements APIService.
 func (srv *Server) HandleVoteRequest(ctx context.Context, req *VoteRequest) (*VoteResponse, error) {
-	var res VoteResponse
-	if req.Term >= srv.Term && srv.state == FOLLOWER_STATE && srv.voteFor == "" {
-		res.Term = req.Term
-		res.Voted = true
+	var resp VoteResponse
+
+	if srv.state == FOLLOWER_STATE && req.Term >= srv.Term && srv.Leader == "" {
+		if srv.voteFor == "" {
+			resp.Term = req.Term
+			srv.Term = req.Term
+			srv.voteFor = req.From.Addr
+			srv.election.Reset(electionTimeout())
+		} else if srv.voteFor == req.From.Addr {
+			srv.voteFor = ""
+			srv.Leader = req.From.Addr
+			srv.election.Stop()
+		}
 	}
-	return &res, nil
+	return &resp, nil
 }
 
 func (srv *Server) HandleAppendLogEntry(ctx context.Context, req *AppendLogRequest) (*AppendLogResponse, error) {
 	if srv.state != FOLLOWER_STATE {
 		return nil, fmt.Errorf("Invalid state")
 	}
-	srv.appendlogChan <- req
-	return &AppendLogResponse{Accepted: true}, nil
-}
+	accepts := 0
+	for _, node := range srv.Others {
+		res, err := node.SendAppendLogRequest(&AppendLogRequest{Commited: false})
+		if err != nil {
+			logger.FromContext(ctx).Errorf("SendAppendLogPrepare(node=%v) error: %v\n", node, err)
+		}
+		if res.Accepted {
+			accepts++
+		}
+	}
+	if accepts >= (len(srv.Others)+1)/2 {
+		for _, node := range srv.Others {
+			node.SendAppendLogRequest(&AppendLogRequest{Commited: true})
+		}
+	}
 
-func (*Server) HandleHealthRequest(req *AppendLogRequest) (*AppendLogResponse, error) {
-	return &AppendLogResponse{Accepted: true}, nil
-}
-
-func (*Server) HandleAppendLogPrepare(req *AppendLogRequest) (*AppendLogResponse, error) {
-	return &AppendLogResponse{Accepted: true}, nil
-}
-
-func (*Server) HandleAppendLogCommit(req *AppendLogRequest) (*AppendLogResponse, error) {
-	return &AppendLogResponse{Accepted: true}, nil
-}
-
-type Node struct {
-	Addr string
-	Name string
-}
-
-type VoteStatus int
-
-const (
-	VoteProgressing VoteStatus = iota
-	VoteWin
-)
-
-func (node *Node) SendVoteRequest(addr string, req *VoteRequest) (*VoteResponse, error) {
-	return &VoteResponse{Peer: &Node{Name: "B"}, Voted: true}, nil
-}
-
-func (node *Node) SendHealthRequest(term int) (*AppendLogResponse, error) {
-	return node.SendAppendLogRequest(&AppendLogRequest{Term: term, Commited: true})
-}
-
-func (node *Node) SendAppendLogRequest(req *AppendLogRequest) (*AppendLogResponse, error) {
 	return &AppendLogResponse{Accepted: true}, nil
 }
 
 func raftLeaderLoop(ctx context.Context, me *Server) error {
 	heartbeatTick := time.Tick(10 * time.Millisecond)
-	log := logger.FromContext(ctx)
 
 	for {
 		select {
@@ -141,9 +154,9 @@ func raftLeaderLoop(ctx context.Context, me *Server) error {
 		case <-heartbeatTick:
 			acks := 0
 			for _, node := range me.Others {
-				_, err := node.SendHealthRequest(me.Term)
+				_, err := node.SendHealthcheckRequest(me.Term)
 				if err != nil {
-					log.Errorf("SendHealthRequest(node=%v) error: %v\n", node, err)
+					logger.FromContext(ctx).Errorf("SendHealthRequest(node=%v) error: %v\n", node, err)
 					continue
 				}
 				acks += 1
@@ -151,36 +164,16 @@ func raftLeaderLoop(ctx context.Context, me *Server) error {
 			if acks == 0 {
 				return fmt.Errorf("None of others response my healthcheck request")
 			}
-
-		case <-me.appendlogChan: // new request from client
-			accepts := 0
-			for _, node := range me.Others {
-				res, err := node.SendAppendLogRequest(&AppendLogRequest{Commited: false})
-				if err != nil {
-					log.Errorf("SendAppendLogPrepare(node=%v) error: %v\n", node, err)
-				}
-				if res.Accepted {
-					accepts++
-				}
-			}
-			if accepts >= (len(me.Others)+1)/2 {
-				for _, node := range me.Others {
-					node.SendAppendLogRequest(&AppendLogRequest{Commited: true})
-				}
-			}
 		}
 	}
 }
 
 func TryElectRaftLeader(ctx context.Context, me *Server) error {
-	votechan := make(chan *VoteRequest)
-	electionTimeout := func() time.Duration { return time.Duration(rand.Int()%151+150) * time.Millisecond }
-	electionTimer := time.NewTimer(electionTimeout())
-
 	for {
 		select {
-		case <-electionTimer.C:
-			electionTimer.Reset(electionTimeout())
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-me.election.C:
 			me.state = CANDIDATE_STATE
 			me.Term += 1
 			me.voteFor = "me"
@@ -199,17 +192,11 @@ func TryElectRaftLeader(ctx context.Context, me *Server) error {
 			}
 			if votes >= (len(me.Others)+1)/2 {
 				me.state = LEADER_STATE
+				me.election.Stop()
 				raftLeaderLoop(ctx, me)
 				me.state = FOLLOWER_STATE
 			}
-		case voteReq := <-votechan:
-			if me.state == FOLLOWER_STATE && me.voteFor == "" {
-				me.voteFor = voteReq.From.Name
-				me.Term = voteReq.Term
-				// Accepted = true
-			} else {
-				// Accepted = false
-			}
+			me.election.Reset(electionTimeout())
 		}
 	}
 }
@@ -223,8 +210,12 @@ func Run(ctx context.Context, state *Server) error {
 	return TryElectRaftLeader(ctx, state)
 }
 
+func electionTimeout() time.Duration {
+	return time.Duration(rand.Int()%151+150) * time.Millisecond
+}
+
 func main() {
-	server := Server{Addr: "A", Others: []*Node{{Addr: "B"}, {Addr: "C"}}, Term: 0}
+	server := Server{Addr: "A", Others: []*Node{{Addr: "B"}, {Addr: "C"}}, Term: 0, election: time.NewTimer(electionTimeout())}
 	ctx := netutil.InitSignalHandler(context.TODO())
 
 	if err := Run(ctx, &server); err != nil {
