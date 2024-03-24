274939fa5086d7e5e277e394212825f62f56954f
yuansl <yuanshenglong@126.com>
2024-03-24 17:48:33 +0800

    cmd/raise-emacs: refactor

--- cmd/raise-emacs/main.c
+++ cmd/raise-emacs/main.c
@@ -1,14 +1,17 @@
-#include "dbus/dbus-protocol.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include <jansson.h>
 #include <dbus/dbus.h>
 
-#define DB_DESTINATION "org.gnome.Shell"
-#define DB_PATH	       "/org/gnome/Shell/Extensions/WindowsExt"
-#define DB_INTERFACE   "org.gnome.Shell.Extensions.WindowsExt"
+#define __unused       __attribute__((unused))
+
+#define DBUS_NAME      "org.gnome.Shell"
+#define DBUS_PATH      "/org/gnome/Shell/Extensions/WindowsExt"
+#define DBUS_INTERFACE "org.gnome.Shell.Extensions.WindowsExt"
 
 struct dbus_context {
 	DBusConnection *conn;
@@ -33,7 +36,10 @@ void dbus_init(struct dbus_context *ctx)
 	}
 }
 
-static DBusMessage *dbus_call_method(struct dbus_context *ctx, DBusMessage *msg)
+typedef DBusMessage dbus_message;
+
+static dbus_message *dbus_call_method(struct dbus_context *ctx,
+				      dbus_message *msg)
 {
 	DBusPendingCall *pending;
 
@@ -70,56 +76,93 @@ void dbus_close(struct dbus_context *ctx)
 	dbus_connection_close(ctx->conn);
 }
 
-#ifdef json_array_foreach
-#undef json_array_foreach
-#define json_array_foreach(array, elem)                               \
-	for (size_t index = 0; index < json_array_size(array) &&      \
-			       (elem = json_array_get(array, index)); \
-	     index++)
+struct wl_window_desc {
+	char *class;
+	char *title;
+	pid_t pid;
+	uint64_t id;
+	bool focus;
+	int64_t maximized;
+};
 
-#endif
-
-static void json_array_foreach_do(json_t *array)
+static void wl_window_pretty(struct wl_window_desc *window)
 {
-	json_t *iter;
+	printf("title=%s,pid=%d,id=%lu,focus=%s,maximized=%ld\n", window->title,
+	       window->pid, window->id, window->focus ? "true" : "false",
+	       window->maximized);
+}
 
-	json_array_foreach (array, iter) {
-		json_t *class = json_object_get(iter, "class");
-		switch ((int)class->type) {
-		case JSON_STRING:
-			if (!strcmp(json_string_value(class), "emacs")) {
-				json_t *title = json_object_get(iter, "title");
-				json_t *pid   = json_object_get(iter, "pid");
-				json_t *id    = json_object_get(iter, "id");
-				json_t *focus = json_object_get(iter, "focus");
-				json_t *maximized =
-					json_object_get(iter, "maximized");
-				printf("title=%s,pid=%lld,id=%lld,focus=%s,maximized=%lld\n",
-				       json_string_value(title),
-				       json_integer_value(pid),
-				       json_integer_value(id),
-				       json_boolean_value(focus) ? "true" :
-								   "false",
-				       json_integer_value(maximized));
-			}
-			break;
-		default:
+static void json_decode_wl_window(json_t *iter, void *v)
+{
+	json_t *class		      = json_object_get(iter, "class");
+	struct wl_window_desc *window = v;
+
+	assert(class != NULL && v != NULL);
+
+	switch (class->type) {
+	case JSON_STRING:
+		if (!strcmp(json_string_value(class), "emacs")) {
+			window->title = (char *)json_string_value(
+				json_object_get(iter, "title"));
+			window->id =
+				json_integer_value(json_object_get(iter, "id"));
+			window->pid = json_integer_value(
+				json_object_get(iter, "pid"));
+			window->focus = json_boolean_value(
+				json_object_get(iter, "focus"));
+			window->maximized = json_integer_value(
+				json_object_get(iter, "maximized"));
+
+			wl_window_pretty(window);
 		}
+		break;
+	default:
 	}
 }
 
-[[maybe_unused]] static void list_active_windows(struct dbus_context *ctx)
+#ifdef json_array_foreach
+#undef json_array_foreach
+
+#define json_array_foreach(array, block)                              \
+	json_t *iter;                                                 \
+	for (size_t index = 0; index < json_array_size(array) &&      \
+			       (iter = json_array_get(array, index)); \
+	     index++)                                                 \
+	block
+
+#endif
+
+static void json_array_foreach_do(json_t *array,
+				  void (*json_decode)(json_t *, void *))
+{
+	json_array_foreach (array, {
+		struct wl_window_desc window;
+
+		json_decode(iter, &window);
+	})
+}
+
+static json_t *json_load(const char *buf)
+{
+	json_t *array	 = NULL;
+	json_error_t err = {};
+
+	array = json_loads(buf, 0, &err);
+	if (!array) {
+		fprintf(stderr, "json_loads error: `%s`\n", err.text);
+		abort();
+	}
+
+	return array;
+}
+
+static void __unused list_active_windows(struct dbus_context *ctx)
 {
 	DBusMessage *msg;
 	DBusMessageIter iter;
 
-	msg = dbus_message_new_method_call(DB_DESTINATION, // target for
-							   // the method
-							   // call
-					   DB_PATH,	   // object to call on
-					   DB_INTERFACE,   // interface to
-							 // call on
-					   "List"); // method name
+	msg = dbus_message_new_method_call(DBUS_NAME, DBUS_PATH, DBUS_INTERFACE,
+					   "List");
 	if (!msg) {
 		fprintf(stderr, "Message Null\n");
 		exit(1);
@@ -130,19 +173,16 @@ static void json_array_foreach_do(json_t *array)
 
 	do {
 		int type = dbus_message_iter_get_arg_type(&iter);
+
 		switch (type) {
 		case DBUS_TYPE_STRING: {
 			const char *buf;
-			dbus_message_iter_get_basic(&iter, &buf);
-			json_error_t err;
-			json_t *array = json_loads(buf, 0, &err);
+			json_t *array;
 
-			if (!array) {
-				fprintf(stderr, "json_loads error: `%s`\n",
-					err.text);
-				abort();
-			}
-			json_array_foreach_do(array);
+			dbus_message_iter_get_basic(&iter, &buf);
+			array = json_load(buf);
+
+			json_array_foreach_do(array, json_decode_wl_window);
 
 			break;
 		}
@@ -161,15 +201,10 @@ static void raise_emacs_window(struct dbus_context *ctx)
 {
 	DBusMessage *msg;
 
-	msg = dbus_message_new_method_call(DB_DESTINATION, // target for
-							   // the method
-							   // call
-					   DB_PATH,	   // object to call on
-					   DB_INTERFACE,   // interface to
-							 // call on
-					   "RaiseEmacsWindow"); // method name
+	msg = dbus_message_new_method_call(DBUS_NAME, DBUS_PATH, DBUS_INTERFACE,
+					   "RaiseEmacsWindow");
 	if (!msg) {
-		fprintf(stderr, "Message Null\n");
+		fprintf(stderr, "null message: %m\n");
 		exit(1);
 	}
 	dbus_call_method(ctx, msg);
@@ -180,7 +215,10 @@ int main(void)
 	struct dbus_context ctx;
 
 	dbus_init(&ctx);
+
 	raise_emacs_window(&ctx);
+
 	dbus_close(&ctx);
+
 	return 0;
 }
